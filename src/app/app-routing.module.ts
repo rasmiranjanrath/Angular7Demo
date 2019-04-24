80d752d8dcda72cf9f9dd2d001ba1716f4eba547
Rasmiranjan Rath <rasmiranjan.rath@girmiti.com>
2019-04-24 11:54:42 +0530

    intitial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
-import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
 
-const routes: Routes = [];
 
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
